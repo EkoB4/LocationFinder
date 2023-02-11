ea1b57a222156741d50e558c59112a54e3dd961f
EkoB4 <ekinbacik@gmail.com>
2023-02-12 02:41:16 +0300

    Initial Commit

---
+++ LocationApp/LocationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationAppApp.swift
+//  LocationApp
+//
+//  Created by Ekin Bacik on 12.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct LocationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
